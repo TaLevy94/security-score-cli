def33f41e81c342f69b8adaff7b28b278b62179b
TaLevy94 <talevy94@gmail.com>
2022-02-06 01:13:23 +0200

    [Add] first cli command

--- src/cli.py
+++ src/cli.py
@@ -1,13 +1,33 @@
 import typer
 from typing import Optional
 
+import config
+from adapters.security_score_api_adapter import SecurityScoreApiAdapter
+
 __app_name__ = "security-score-cli"
 __version__ = "0.1.0"
 
 
 app = typer.Typer()
 
-@app.command()
-def set_access_token():
-    print("hi")
+security_score_api_client = SecurityScoreApiAdapter(api_url=config.API_URL,api_port=config.API_PORT)
 
+@app.command()
+def scan_trending_repos(access_token: str = typer.Option(...,"--access-token","-t", \
+    help="Access token in ghp format, see: https://docs.github.com/en/authentication/ \
+        keeping-your-account-and-data-secure/creating-a-personal-access-token"), \
+        count:int= typer.Option(...,"--cout","-c", help="number of reposiroties to scan")):
+    repos_data = security_score_api_client.get_trending_repos_secure_score(access_token=access_token,count=count)
+    print(repos_data)
+
+@app.command()
+def version():
+    typer.echo(f"My superCLI Version: {__version__}")
+
+@app.callback()
+def callback():
+    """
+    Code security measurement CLI tool.
+
+    Wait for future versions and alot more insights 
+    """
--- src/config.py
+++ src/config.py
@@ -1,3 +1,5 @@
 import os
 
-API_URL = os.get_env("API_URL", "http://localhost")
+API_URL = os.getenv("API_URL", "http://localhost")
+
+API_PORT = os.getenv("API_PORT", 5000)
